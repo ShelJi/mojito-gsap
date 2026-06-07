98a01674062fedaf8b4d0e603f76abd289375fa3
shelj-dev <220747849+shelj-dev@users.noreply.github.com>
2026-06-07 12:20:38 +0530

    hero created

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    <h1>Welcome visaaaaaaaaaaaaaaaa, haideeeeeeeeeee</h1>
+    <h1 className="text-3xl underline">Welcome visaaaaaaaaaaaaaaaa, haideeeeeeeeeee</h1>
     </>
   )
 }
